593861f3e296cf6ff2df2b371ea80076926d461a
chinmay <chinmay2003cp@gmail.com>
2021-08-24 15:01:41 +0530

    Created Admin login page and admin table in database

--- PHP/Login.php
+++ PHP/Login.php
@@ -1,45 +1,24 @@
 <?php
 
-session_start();
 include 'Config.php';
-function alert($msg) {
-        echo "<script type='text/javascript'>alert('$msg')</script>";
-    }
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    function validate($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
 
-    $username = validate($_POST['username']);
-    $password = validate($_POST['password']);
-    if (empty($username)) {
-        alert('Username required');
-    }
-    elseif (empty($password)) {
-        alert('Password required');
-    }
-    else {
-        $sql = "SELECT * FROM stud_details WHERE username = '$username' AND password = '$password'";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $username && $row['password'] === $password) {
-                header('Location: Home.html');
-            }
-            else {
-                alert('Incorrect Username or Password');
-            }
-        }
-        else {
-            alert('Incorrect Username or Password');
-        }
-    }
+$sql = "SELECT * FROM stud_details WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+
+if ($count == 1) {
+    header('Location: Home.html');
+} else {
+    header('Location: Login.html');
 }
-header('Location: Home.html');
 
 
 ?>
