f15419c9e0261cfe00e38fcbe2afd871e1f1fa2d
chinmay <chinmay2003cp@gmail.com>
2021-09-15 23:22:55 +0530

    Added show hide functionality to reply button for better appearance

--- PHP/adminQueryList.php
+++ PHP/adminQueryList.php
@@ -32,7 +32,8 @@
                     <button type="submit" name="submit" value="Submit Form" class="delete" onclick="del()"><i class="fa fa-trash-o fa-2x" id="del-btn"></i></button>
                     <input type="hidden" name="id" value="<?php echo $row['id'];?>">
                 </form>
-                <form action="reply.php" method="post">
+                <div class="rep-show" data-id="<?php echo $row['id'];?>" onclick="show(this)">Reply</div>
+                <form action="reply.php" method="post" style="display: none;" id="form-<?php echo $row['id'];?>">
                     <textarea id="insert-text" name="reply" value="<?php echo $row['reply'];?>"></textarea>   
                     <button type="submit" name="submit" value="Submit Form" class="ok-btn">OK</button>
                     <input type="hidden" name="id" value="<?php echo $row['id'];?>">
@@ -43,6 +44,15 @@
     <script>
         function del() {
             alert("Are you sure you want to delete this query?")
+        }
+
+        function show(self) {
+            var commentId = self.getAttribute("data-id");
+            if (document.getElementById("form-" + commentId).style.display == "") {
+                document.getElementById("form-" + commentId).style.display = "none";
+            } else {
+                document.getElementById("form-" + commentId).style.display = "";
+            }
         }
     </script>
 </body>
